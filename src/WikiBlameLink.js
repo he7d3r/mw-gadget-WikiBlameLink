ae19f9b984fc9746a5fcf7c1a1a28ff602a45957
Helder <he7d3r@gmail.com>
2012-03-17 19:17:02 +0000

    Add a link to WikiBlame
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/WikiBlameLink.js
@@ -0,0 +1,38 @@
+/**
+ * Add a link to WikiBlame
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/WikiBlameLink.js]] ([[File:User:Helder.wiki/Tools/WikiBlameLink.js]])
+ */
+/*jslint browser: true, white: true, devel: true, regexp: true */
+/*global jQuery, mediaWiki */
+( function ( $, mw /* , undefined */ ) {
+'use strict';
+
+function addWikiBlameLink(){
+	$( mw.util.addPortletLink(
+		'p-cactions',
+		'#',
+		'WikiBlame',
+		'ca-blame',
+		'Identificar o autor de um trecho da página, usando o WikiBlame'
+	)).click( function( e ) {
+		e.preventDefault();
+		var tip = 'Digite um texto no campo abaixo para saber quem o incluiu na página atual.',
+			url = 'http://wikipedia.ramselehof.de/wikiblame.php?',
+			data = {
+				'article': mw.config.get('wgPageName'),
+				'user_lang': mw.config.get('wgUserLanguage').replace(/-.+/g, ''),
+				'lang': mw.config.get('wgContentLanguage'),
+				'needle': prompt(tip, 'Texto'),
+				'project': mw.config.get('wgServer')
+					.replace( /\/\/[a-z]+\.([a-z]+).org/, '$1' )
+			};
+		window.open( url + $.param( data ), '_blank');
+	});
+}
+
+if ( mw.config.get( 'wgNamespaceNumber' ) >= 0 ) {
+	$(addWikiBlameLink);
+}
+
+}( jQuery, mediaWiki ) );
